d99e74f277ca7e7b42c9d28f9791069a14c40d00
Natallia Adams <milanaadams@gmail.com>
2021-05-28 11:28:52 +0300

    feat: task 4 part 1 solution

--- src/errors/handleErrors.js
+++ src/errors/handleErrors.js
@@ -1,3 +1,10 @@
+/**
+ * Middleware for handling errors
+ * @param {Error} err 
+ * @param {Request} req 
+ * @param {Response} res 
+ * @param {NextFunction} next 
+ */
 function handleErrors(err, req, res, next) {
   if (!res.headersSent) {
     res.status(err.status || 500).json({ message: err.message || 'Something went went' });
--- src/utils/dbUtils.js
+++ src/utils/dbUtils.js
@@ -1,19 +1,43 @@
 const { db } = require('./inMemoryDB');
 
+/**
+ * Returns all entities from the specified table
+ * @param {string} tableName DB table name
+ * @returns {object[]} Array of objects
+ */
 function getAllEntities(tableName) {
   return db[tableName].filter(entry => entry);
 }
 
+/**
+ * Gets entity by its id from the specified table
+ * @param {string} tableName DB table name
+ * @param {uuidv4} id entity id
+ * @returns {object} object with entity's properties
+ */
 function getEntity(tableName, id) {
   const entity = db[tableName].filter(entry => entry.id === id);
   return entity[0];
 }
 
+/**
+ * Creates new entity and saves it to the specified table
+ * @param {string} tableName DB table name
+ * @param {object} entity object with entity's properties
+ * @returns {object} object with entity's properties
+ */
 function createEntity(tableName, entity) {
   db[tableName].push(entity);
   return getEntity(tableName, entity.id);
 }
 
+/**
+ * Updates entity by its id in the specified table
+ * @param {string} tableName DB table name
+ * @param {uuidv4} id entity id
+ * @param {object} entity entity's properties to be updated
+ * @returns {object} updated object with entity's properties
+ */
 function updateEntity(tableName, id, entity) {
   const entityToUpdate = getEntity(tableName, id);
   if(entityToUpdate) {
@@ -23,6 +47,12 @@ function updateEntity(tableName, id, entity) {
   return getEntity(tableName, id);
 }
 
+/**
+ * Remove entity from the specified table
+ * @param {string} tableName DB table name
+ * @param {uuidv4} id entity id
+ * @returns {object} deleted entity
+ */
 function deleteEntity(tableName, id) {
   const table = db[tableName];
   const entity = getEntity(tableName, id);
@@ -34,18 +64,37 @@ function deleteEntity(tableName, id) {
   return entity;
 }
 
+/**
+ * Returns all entities from specified table with given boardId
+ * @param {string} tableName DB table name
+ * @param {uuidv4} boardId board id
+ * @returns {object[]} Array of entities wuth the speified boardId
+ */
 function getAllEntitiesByBoardId(tableName, boardId) {
   const table = db[tableName];
   const entities = table.filter(entry => entry.boardId === boardId);
   return entities;
 }
 
+/**
+ * Get entity from a specified table by id and boardId
+ * @param {string} tableName DB table name
+ * @param {uuidv4} boardId board id
+ * @param {uuidv4} id entity id
+ * @returns {object} entity from the specified table
+ */
 function getEntityByBoardId(tableName, boardId, id) {
   const entities = getAllEntitiesByBoardId(tableName, boardId);
   const entity = entities.filter(entry => entry.id === id)[0];
   return entity;
 }
 
+/**
+ * Get all entities from the specified table by user id
+ * @param {string} tableName DB table name
+ * @param {uuidv4} userId user id
+ * @returns {object} entity that includes the specified user id
+ */
 function getAllEntitiesByUserId(tableName, userId) {
   const table = db[tableName];
   const entities = table.filter(entry => entry.userId === userId);
